22d9c0344c0cfbab359cd9a624892987c12c4e07
Rodrigo Dias <rodrigodcontato@gmail.com>
2025-05-22 23:59:50 -0300

    fix vercel build v.6

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './Styles/global.css'
+import './Styles/Global.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
